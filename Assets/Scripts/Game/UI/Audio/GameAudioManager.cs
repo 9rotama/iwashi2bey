035fe6434ed4386aca42b7ac2d60aacec513946a
potekumakun <yukidaisensei@gmail.com>
2022-03-19 19:21:53 +0900

    音声（仮）を追加

---
+++ Assets/Scripts/Game/UI/Audio/GameAudioManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudioManager : MonoBehaviour
+{
+    //SE
+
+    //コマの衝突音
+    static AudioSource s_hitSe;
+    [SerializeField] AudioSource _hitSe;
+
+    //ボタンの押したときの音
+    static AudioSource s_buttonPushSe;
+    [SerializeField] AudioSource _buttonPushSe;
+
+    //勝利音
+    static AudioSource s_winSe;
+    [SerializeField] AudioSource _winSe;
+
+    //敗北音
+    static AudioSource s_loseSe;
+    [SerializeField] AudioSource _loseSe;
+
+    //カウントダウン
+    static AudioSource s_countDownSe;
+    [SerializeField] AudioSource _countDownSe;
+
+
+    //BGM
+
+    //タイトルBGM
+    static AudioSource s_titleBgm;
+    [SerializeField] AudioSource _titleBgm;
+
+    //ゲームBGM
+    static AudioSource s_gameBgm;
+    [SerializeField] AudioSource _gameBgm;
+
+
+
+    void Awake()
+    {
+        s_hitSe = _hitSe;
+        s_buttonPushSe = _buttonPushSe;
+        s_titleBgm = _titleBgm;
+    }
+
+    //SE
+    public static void PlayHitSe()
+    {
+        s_hitSe.PlayOneShot(s_hitSe.clip);
+    }
+
+    public static void PlayPushButtonSe()
+    {
+        s_buttonPushSe.Play();
+    }
+
+
+    public static void PlayWinSe()
+    {
+        s_winSe.Play();
+    }
+
+    public static void PlayLoseSe()
+    {
+        s_loseSe.Play();
+    }
+
+    //BGM
+    public static void PlayTitleBgm()
+    {
+        s_titleBgm.Play();
+    }
+
+    public static void PlayGameBgm()
+    {
+        s_gameBgm.Play();
+    }
+
+
+
+}
